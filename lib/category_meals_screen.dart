687394eb255be47df311b7bc7f3cda9be0bd5824
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-01-14 19:23:14 +0200

    filter meals by cat. id

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:deli_meals/dummy_data.dart";
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = "/category-meals";
@@ -17,13 +18,19 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryId = routeArgs["id"];
     final categoryTitle = routeArgs["title"];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
       body: Center(
-        child: Text(
-          "Catergory Meals Screen",
+        child: ListView.builder(
+          itemBuilder: (ctx, i) {
+            return Text(categoryMeals[i].title);
+          },
+          itemCount: categoryMeals.length,
         ),
       ),
     );
