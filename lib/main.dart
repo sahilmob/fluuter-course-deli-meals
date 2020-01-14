d70f14c7da95d08da218cb2b2d333458b4ea943c
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-01-14 19:00:22 +0200

    use static propery for route names

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,9 @@ class MyApp extends StatelessWidget {
             ),
       ),
       home: CategoriesScreen(),
-      routes: {"/category-meals": (ctx) => CategoryMealsScreen()},
+      routes: {
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
